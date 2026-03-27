583aded9f867d855670dd18c22ee3df488caa311
MTV2007 <marinavigil53@gmail.com>
2026-03-27 15:13:27 -0600

    My finished product!

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,91 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.Random;
+import java.util.Scanner;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        out.println("Welcome to the Magic 8 Ball!");
+        String Ball = "      _......._\n" +
+                "      .-:::::::::::-.\n" +
+                "    .:::::::::::::::::.\n" +
+                "   :::::::' .-. `:::::::\n" +
+                "  :::::::  :   :  :::::::\n" +
+                " ::::::::  :   :  ::::::::\n" +
+                " :::::::::._`-'_.:::::::::\n" +
+                " :::::::::' .-. `:::::::::\n" +
+                " ::::::::  :   :  ::::::::\n" +
+                "  :::::::  :   :  :::::::\n" +
+                "   :::::::._`-'_.:::::::\n" +
+                "    `:::::::::::::::::'\n" +
+                "jgs   `-:::::::::::-'\n" +
+                "         `'''''''`";
+        out.println(Ball);
+
+        while(true) {
+            out.println("What question would you like to ask the Oracle?");
+
+            Scanner scanner = new Scanner(in);
+            String Input = scanner.nextLine();
+            out.println("You asked: " + Input);
+
+            String Answer = get8BallAnswer();
+            out.println(Answer);
+        }
+    }
+
+    private static String get8BallAnswer() {
+        String [] magicresponse = {
+                "It is certain",
+                "It is decidedly so",
+                "Without a doubt",
+                "Yes definitely",
+                "You may rely on it",
+                "As, I see it, yes",
+                "Most likely",
+                "Outlook good",
+                "Yes",
+                "Signs point to yes",
+                "Reply hazy, try again",
+                "Ask again later",
+                "Better not tell you now",
+                "Cannot predict now",
+                "Concentrate and ask again",
+                "Don't count on it",
+                "My reply is no",
+                "My sources say no",
+                "Outlook not so good",
+                "Very doubtful"
+        };
+        Random random = new Random();
+        out.println("        _.---.__\n" +
+                "      .'        `-.\n" +
+                "     /      .--.   |\n" +
+                "     \\/  / /    |_/\n" +
+                "      `\\/|/    _(_)\n" +
+                "   ___  /|_.--'    `.   .\n" +
+                "   \\  `--' .---.     \\ /|\n" +
+                "    )   `       \\     //|\n" +
+                "    | __    __   |   '/||\n" +
+                "    |/  \\  /  \\      / ||\n" +
+                "    ||  |  |   \\     \\  |\n" +
+                "    \\|  |  |   /        |\n" +
+                "   __\\\\@/  |@ | ___ \\--'\n" +
+                "  (     /' `--'  __)|\n" +
+                " __>   (  .  .--' &\"\\\n" +
+                "/   `--|_/--'     &  |\n" +
+                "|                 #. |\n" +
+                "|                 q# |\n" +
+                " \\              ,ad#'\n" +
+                "  `.________.ad####'\n" +
+                "    `#####\"\"\"\"\"\"''\n" +
+                "     `&#\"\n" +
+                "      &# \"&\n" +
+                "      \"#ba\"");
+        out.println("Hmm...");
+
+        int indexLookUp = random.nextInt(magicresponse.length);
+        out.println(magicresponse[indexLookUp]);
+        return "";
+    }
 }
